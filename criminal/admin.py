1e03cff99dab8353c3648524cf6437001b7cecda
henriqueespadas <henriqueep@hotmail.com>
2024-07-05 22:45:08 -0300

    [ADD] Css tela registro

--- criminal/admin.py
+++ criminal/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
 from .models.criminal import Criminal
-from .models.crime import Crime
-from .models.inventory import Inventory
 from .models.classification import Classification
 
 admin.site.register(Criminal)
